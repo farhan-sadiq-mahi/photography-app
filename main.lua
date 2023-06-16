6f0aebdd95dcd7b3f4bf12870fafa4b8c624acda
MD. Farhan Sadiq <108167433+farhan-sadiq-mahi@users.noreply.github.com>
2023-06-16 17:30:31 +0600

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,30 @@
+local StorageKey = "AlreadyExecuted"
+
+local function executeScript(scriptUrl)
+    local script = game:HttpGet(scriptUrl, true)
+    local executeFunction = loadstring(script)
+    executeFunction()
+end
+
+local scripts = {
+    "https://raw.githubusercontent.com/farhan-sadiq-mahi/photography-app/main/luaz/design.lua",
+    "https://raw.githubusercontent.com/farhan-sadiq-mahi/photography-app/main/rpc.lua",
+    "https://raw.githubusercontent.com/farhan-sadiq-mahi/photography-app/main/txt.lua",
+    "https://raw.githubusercontent.com/farhan-sadiq-mahi/photography-app/main/luaz/main.lua"
+}
+
+if not _G[StorageKey] then
+    for i, scriptUrl in ipairs(scripts) do
+        if i == 2 then
+            wait(6) -- Delay for 6 seconds before loading the second script
+        end
+
+        coroutine.wrap(function()
+            executeScript(scriptUrl)
+        end)()
+    end
+    
+    _G[StorageKey] = true
+else
+    print("Script has already been executed.")
+end
