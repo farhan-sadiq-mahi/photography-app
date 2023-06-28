fb5dbe336f7418966fc0184b4708a2558aa7edc9
MD. Farhan Sadiq <108167433+farhan-sadiq-mahi@users.noreply.github.com>
2023-06-28 15:16:21 +0600

    Add files via upload

--- ded.lua
+++ ded.lua
@@ -1,12 +1,6 @@
--- Get the player's character
-local player = game.Players.LocalPlayer
-local character = player.Character
-
 -- Function to kill the character
-local function killCharacter()
-    -- Check if the character exists
+local function killCharacter(character)
     if character then
-        -- Kill the character
         local humanoid = character:FindFirstChild("Humanoid")
         if humanoid then
             humanoid.Health = 0
@@ -14,8 +8,30 @@ local function killCharacter()
     end
 end
 
--- Loop to kill the character every 9 seconds
-while true do
-    wait(6)
-    killCharacter()
+-- Function to generate a random interval
+local function getRandomInterval()
+    local intervals = {60, 140, 102, 120}
+    local randomIndex = math.random(1, #intervals)
+    return intervals[randomIndex]
+end
+
+-- Function to repeatedly kill the character at random intervals
+local function repeatKill(character)
+    while true do
+        wait(getRandomInterval())
+        killCharacter(character)
+    end
+end
+
+-- Get the player's character
+local player = game.Players.LocalPlayer
+
+-- Listen for character added event
+player.CharacterAdded:Connect(function(character)
+    repeatKill(character)
+end)
+
+-- Check if the character already exists
+if player.Character then
+    repeatKill(player.Character)
 end
